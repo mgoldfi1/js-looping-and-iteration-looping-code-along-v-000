bbb67b0410adc88bcad2a27416848092caa9b0ad
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 16:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+
+  function printBadges(badges) {
+    for (let i = 0; i < badges.length; i++) {
+      console.log(`Welcome ${badges[i]}!, You are employee #${i+1}`)
+    }
+  }
