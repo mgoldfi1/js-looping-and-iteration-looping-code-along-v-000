e1e860799a9bf4e17b91c684cc790256b38d01ca
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 16:41:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,11 @@
   }
 
   function tailsNeverFails() {
-
-    // let x = 0
-    //   while (Math.random() <= 0.5) {
-    //     x++;
-    //     console.log(`You got ${x} tails in a row!`)
-    //
-    //   }
+    let flip = Math.random()
+    let x = 0
+      while (flip <= 0.5) {
+        x++;
+        console.log(`You got ${x} tails in a row!`)
+    
+      }
   }
